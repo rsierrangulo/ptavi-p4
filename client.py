f0fa556d8ccdf2b9ae32c3faa0ae587e3408bd9a
ignacio malo segura <imalo@iota25.aulas.gsyc.es>
2014-10-20 10:29:05 +0200

    ej4

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
 
 import socket
-
+import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2])
+comandos = sys.argv
+linea = comandos[3:]
+linea = " ".join(linea)
+print linea
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = linea
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
