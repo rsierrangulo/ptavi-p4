862b9a41b64a087a6fba9d0c68021ddb8474ff1c
raquel sierra angulo <rsierra@kappa20.aulas.gsyc.es>
2014-10-22 13:40:54 +0200

    Ejercicio 7 sin pep8

--- client.py
+++ client.py
@@ -9,29 +9,31 @@ import sys
 
 comandos = sys.argv
 
-# Cliente UDP simple.
 
-# Dirección IP del servidor.
+if len(comandos) != 6:
+    sys.exit ('Usage: client.py ip puerto register sip_address expires_value') 
+
 SERVER = comandos[1]
 PORT = int(comandos[2])
 PETICION = comandos[3]
-
-# Contenido que vamos a enviar
 DIRECCION = comandos[4]
+TIEMPO = comandos[5]
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + PETICION + " sip: " + DIRECCION
 
-my_socket.send(PETICION + " sip: " + DIRECCION  + " SIP/1.0\r\n\r\n")
+
+print "Enviando: " + PETICION + " sip: " + DIRECCION + '\r\n' + "Expires: " + TIEMPO
+
+my_socket.send(PETICION + " sip: " + DIRECCION  + " SIP/1.0 " + '\r\n' + "Expires: " + TIEMPO + '\r\n\r\n')
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
 print "Terminando socket..."
 
-# Cerramos todo
+
 my_socket.close()
-print "Fin."
+print "Fin." 
