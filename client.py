f51f37d38e07cfb9b67be8daf87769e1edbf2031
ignacio malo segura <imalo@iota18.aulas.gsyc.es>
2014-10-27 13:29:54 +0100

    ej8

--- client.py
+++ client.py
@@ -6,27 +6,33 @@ import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-comandos = sys.argv
-linea = comandos[3:]
-linea = " ".join(linea)
-print linea
-# Contenido que vamos a enviar
-LINE = linea
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    comandos = sys.argv
+    linea = comandos[3:6]
+    expires = linea[2]
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    if linea[0] == "register":
+        LINE = linea[0].upper() + " sip:" + linea[1] + " SIP/2.0" + "\r\n"
+        LINE = LINE + "Expires: " + expires + "\r\n\r\n"
+        #print LINE
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+    # Contenido que vamos a enviar
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+    print "Enviando: " + LINE
+    my_socket.send(LINE)
+    data = my_socket.recv(1024)
 
-# Cerramos todo
-my_socket.close()
-print "Fin."
+    print 'Recibido -- ', data
+    print "Terminando socket..."
+
+    # Cerramos todo
+    my_socket.close()
+    print "Fin."
+except IndexError:
+    print "Usage: client.py ip puerto register sip_address expires_value"
