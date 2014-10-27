f2452ae13e1f40bed4fac6c2586c46e1a1f6e8f6
raquel sierra angulo <rsierra@iota32.aulas.gsyc.es>
2014-10-27 20:00:59 +0100

    servidor finalizado

--- server.py
+++ server.py
@@ -21,29 +21,41 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     lista_usuario = []
 
     def register2file(self):
+        """
+        Método que imprime en el fichero el contenido del diccionario
+        """
         fichero = open('registered.txt', 'w')
-        fichero.write("User" + '\t\t\t\t' + "IP" + '\t\t\t' + "Expires" + '\r\n')
+        fichero.write("User" + '\t\t\t' + "IP" + '\t\t\t' + "Expires" + '\r\n')
         for usuario in self.diccionario.keys():
-            fichero.write(usuario + '\t' + self.diccionario[usuario][0] + '\t' + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(self.diccionario[usuario][1])) + '\r\n')
+            # El user se imprime como "sip:usuario" debido a que se guarda así
+            # en la lista
+            fichero.write(usuario + '\t' + self.diccionario[usuario][0] + '\t')
+            hora = time.gmtime(self.diccionario[usuario][1])
+            fichero.write(time.strftime('%Y-%m-%d %H:%M:%S', hora) + '\r\n')
         fichero.close()
 
     def handle(self):
+        """
+        Método handle
+        """
         while 1:
             line = self.rfile.read()
             print "Peticion del cliente: " + line
             lista = line.split(" ")
+            print lista
             if lista[0] == "REGISTER":
-                tiempo = time.time() + float(lista[5])
+                tiempo = time.time() + float(lista[3])
                 tiempo_actual = time.time()
                 self.lista_usuario = [self.client_address[0], tiempo]
-                self.diccionario[lista[2]] = self.lista_usuario
+                self.diccionario[lista[1]] = self.lista_usuario
                 for usuario in self.diccionario.keys():
                     if self.diccionario[usuario][1] < tiempo_actual:
                         del self.diccionario[usuario]
-                    self.wfile.write(lista[3] + " 200 OK" + "\r\n\r\n")
-                print lista[3] + " 200 OK" + "\r\n\r\n"
+                    self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+                print "SIP/2.0 200 OK\r\n\r\n"
             else:
-                print "La petición introducida no es válida" + '\r\n'
+                print "SIP/2.0 400 Bad Request\r\n\r\n"
+                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
             self.register2file()
             if not line:
                 break
