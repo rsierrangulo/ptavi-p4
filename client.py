69d6c1e517b1b94b19df8f3905c22120b5e66a3a
raquel sierra angulo <rsierra@iota26.aulas.gsyc.es>
2014-10-27 13:05:52 +0100

    client.py finalizado sin pep8

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ comandos = sys.argv
 
 
 if len(comandos) != 6:
-    sys.exit ('Usage: client.py ip puerto register sip_address expires_value') 
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
 SERVER = comandos[1]
 PORT = int(comandos[2])
@@ -24,11 +24,9 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-
-
 print "Enviando: " + PETICION + " sip: " + DIRECCION + '\r\n' + "Expires: " + TIEMPO
 
-my_socket.send(PETICION + " sip: " + DIRECCION  + " SIP/2.0 " + '\r\n' + "Expires: " + TIEMPO + '\r\n\r\n')
+my_socket.send(PETICION + " sip: " + DIRECCION + " SIP/2.0 " + '\r\n' + "Expires: " + TIEMPO + '\r\n\r\n')
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
@@ -36,4 +34,4 @@ print "Terminando socket..."
 
 
 my_socket.close()
-print "Fin." 
+print "Fin."
