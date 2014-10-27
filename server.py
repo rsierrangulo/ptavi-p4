be38788162e60d1140e127f041e81c5a76e14e01
ignacio malo segura <imalo@iota18.aulas.gsyc.es>
2014-10-27 13:59:03 +0100

    Vpep

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor SIP
-en UDP simple
+Clase (y programa principal) para un servidor de registro SIP
 """
 
 import SocketServer
@@ -22,16 +21,19 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
         cadena = "User\tIP\tExpires\r\n"
 
         for cliente in self.clientes.keys():
+            #Ponemos la hora en el formato deseado
             hora = time.strftime('%Y-%m-%d %H:%M:%S',
                                  time.gmtime(self.clientes[cliente][1]))
             cadena += (cliente + "\t" + self.clientes[cliente][0] +
                        "\t" + hora + "\r\n")
+        #Escribimos el diccionario en el fichero
         fichero.write(cadena)
 
     def handle(self):
         """ Registra y borra clientes del server"""
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
+            # Leemos las lineas del fichero
             line = self.rfile.read()
             if not line:
                 break
@@ -40,9 +42,11 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             direccion = direccionsip.split(":")
             expires = palabras[4]
             horalim = time.time() + float(expires)
+            # Añadimos una entrada al diccionario
             self.clientes[direccion[1]] = (self.client_address[0], horalim)
             vsip = palabras[2]
             print vsip + " 200 OK \r\n\r\n"
+            # Borramos si expires = 0 o el registro ha caducado
             if expires == "0":
                 del self.clientes[(direccion[1])]
             for cliente in self.clientes.keys():
