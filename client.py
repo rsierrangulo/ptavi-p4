297b10404f80b6cb7a927f1e7118a1f58d3f899c
rsierrangulo <rsierrangulo@gmail.com>
2014-10-29 15:26:47 +0100

    modificación cliente
    Te he modificado la excepción cambiándola por un if  que refleje la posibilidad de que el número de comandos que metes por la pantalla sea distinto de 6 para que en ese caso se muestre el mensaje de error, ya que con la excepción podrías contemplar otro tipo de errores que no nos interesan en esta práctica. Además, en el primer docstring te he puesto bien la palabra "registra" no es nada importante pero tenías escrito "registtra".

--- client.py
+++ client.py
@@ -3,32 +3,33 @@
 
 import socket
 import sys
-""" Cliente que se registtra en el servidor """
-try:
-    SERVER = sys.argv[1]
-    PORT = int(sys.argv[2])
-    comandos = sys.argv
-    linea = comandos[3:6]
-    expires = linea[2]
-    # Creamos el contenido que vamos a enviar
-    if linea[0] == "register":
-        LINE = linea[0].upper() + " sip:" + linea[1] + " SIP/2.0" + "\r\n"
-        LINE = LINE + "Expires: " + expires + "\r\n\r\n"
+""" Cliente que se registra en el servidor """
 
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+if len(comandos) != 6:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
+    
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2])
+comandos = sys.argv
+linea = comandos[3:6]
+expires = linea[2]
+# Creamos el contenido que vamos a enviar
+if linea[0] == "register":
+    LINE = linea[0].upper() + " sip:" + linea[1] + " SIP/2.0" + "\r\n"
+    LINE = LINE + "Expires: " + expires + "\r\n\r\n"
 
-    print "Enviando: " + LINE
-    my_socket.send(LINE)
-    data = my_socket.recv(1024)
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((SERVER, PORT))
 
-    print 'Recibido -- ', data
-    print "Terminando socket..."
+print "Enviando: " + LINE
+my_socket.send(LINE)
+data = my_socket.recv(1024)
 
-    # Cerramos todo
-    my_socket.close()
-    print "Fin."
-except IndexError:
-    print "Usage: client.py ip puerto register sip_address expires_value"
+print 'Recibido -- ', data
+print "Terminando socket..."
+
+# Cerramos todo
+my_socket.close()
+print "Fin."
